700b23396b73f7944b1eca55d8ebdb1799451f60
Fares <119789181+FaresYigeremu@users.noreply.github.com>
2024-10-01 00:06:58 +0300

    slight adjustment

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return [...reportWithIterator].join(" | ");
+  const employees = [];
+
+  for (const employ of reportWithIterator) {
+    employees.push(employ);
+  }
+
+  return employees.join(" | ");
 }
