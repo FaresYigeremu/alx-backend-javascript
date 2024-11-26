e0d0991a7b14f636a88e42966cf5ee688a17d0d9
Fares <faresyigeremu@gmail.com>
2024-11-26 16:37:46 +0000

    nodejs

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,11 @@
-// 0-console.js
-function displayMessage(message) {
-    console.log(message);
-  }
-  
-  module.exports = displayMessage;
-  
+/**
+* Function to print a message in the console
+* @param {string} message - Message to print
+*/
+
+function displayMessage(str) {
+  console.log(str); // Print the message to the console
+}
+
+// Export function to make it available to other modules
+module.exports = displayMessage;
--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,13 @@
-// 1-stdin.js
-// Display the welcome message
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Set up stdin (standard input) to receive user input
-process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
 
-// Listen for user input
-process.stdin.on('data', (data) => {
-  const input = data.trim(); // Remove any extra spaces or newlines
-  console.log(`Your name is: ${input}`);
-
-  // End the program
-  process.stdin.end();
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-// Handle the end of input
 process.stdin.on('end', () => {
-  console.log("This important software is now closing");
+  process.stdout.write('This important software is now closing\n');
 });
-
